1b72f15a1c9fc02ac2baf8249561013543245538
YuSung Lee <project401ho@gmail.com>
2021-07-08 15:18:47 +0900

    7.08

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,11 +1,10 @@
 import React, {useState} from 'react'
-import styles from '../../styles/Home.module.css'
 
 export default function Product(props) {
   const [test,setTest] = useState("")
 
   return (
-    <div className={styles.Product_container}>
+    <div>
       
     </div>
   );

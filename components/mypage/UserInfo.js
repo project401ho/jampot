48af923a56fbd349bd1af88ad3ade828cc154330
YuSung Lee <project401ho@gmail.com>
2021-07-10 09:55:58 +0900

    7.10

--- components/mypage/UserInfo.js
+++ components/mypage/UserInfo.js
@@ -10,7 +10,7 @@ export default function UserInfo(props) {
     <div className='UserInfo_container'>
       <Image
         priority
-        src="public/images/profile.png"
+        src="../images/profile.png"
         className={styles.borderCircle}
         height={80}
         width={80}
